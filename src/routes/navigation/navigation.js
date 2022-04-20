5b05eadccc12b288ff5535ff6dac207d096fc6e0
Tehreem Fazili <tehreemfazili5@gmail.com>
2022-04-20 11:47:42 +0530

    updated coponents, updated routes

---
+++ src/routes/navigation/navigation.js
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.scss";
+
+const Shop = () => <div>Hello world</div>;
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div class="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
